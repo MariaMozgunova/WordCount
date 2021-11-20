43fe55b7b8916abf5f6554645331ea57d311620e
Maria Mozgunova <bagira_2003@inbox.ru>
2021-11-20 09:57:40 +0300

    New function solve

--- WordCount.c
+++ WordCount.c
@@ -36,11 +36,7 @@ void parse_args(int argc, char* argv[]) {
     }    
 }
 
-int main(int argc, char* argv[]) {
-    parse_args(argc, argv);
-    const char* fname = argv[argc - 1];
-    FILE* fp = fopen(fname, "r");
- 
+void solve(FILE* fp, int res[]) {
     int bytes = 0;
     int lines = 0;
     int words = 0;
@@ -64,16 +60,28 @@ int main(int argc, char* argv[]) {
         words++;
     }
 
-    if (B == 1) {
-        printf("Number of bytes: %d\n", bytes);
-    } 
-    if (L == 1) {
-        printf("Number of lines: %d\n", lines);
-    } 
-    if (W == 1) {
-        printf("Number of words: %d\n", words);
-    }
-    
-    fclose(fp);
-    return 0;
+    res[0] = bytes;
+    res[1] = lines;
+    res[2] = words;
 }
+
+// int main(int argc, char* argv[]) {
+//     parse_args(argc, argv);
+//     const char* fname = argv[argc - 1];
+//     FILE* fp = fopen(fname, "r");
+//     int res[3];
+//     solve(fp, res);
+
+//     if (B == 1) {
+//         printf("Number of bytes: %d\n", res[0]);
+//     } 
+//     if (L == 1) {
+//         printf("Number of lines: %d\n", res[1]);
+//     } 
+//     if (W == 1) {
+//         printf("Number of words: %d\n", res[2]);
+//     }
+    
+//     fclose(fp);
+//     return 0;
+// }
